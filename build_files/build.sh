e8031a2d50f70b3f4d2394fcdb77d311f726ccc1
Christian Hagenest <c.hagenest@pm.me>
2025-03-27 12:28:39 +0100

    remove sudo from build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,5 +4,5 @@ set -ouex pipefail
 
 # Install Rancher Desktop
 
-sudo dnf config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/isv:/Rancher:/stable/fedora/isv:Rancher:stable.repo
-sudo dnf install rancher-desktop
+dnf config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/isv:/Rancher:/stable/fedora/isv:Rancher:stable.repo
+dnf install rancher-desktop
