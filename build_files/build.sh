89be26fea4019fb8286ffc647ed77a4752786847
Christian Hagenest <c.hagenest@pm.me>
2025-03-27 12:33:32 +0100

    add -y to dnf install

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,4 +5,4 @@ set -ouex pipefail
 # Install Rancher Desktop
 
 dnf config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/isv:/Rancher:/stable/fedora/isv:Rancher:stable.repo
-dnf install rancher-desktop
+dnf install -y rancher-desktop
